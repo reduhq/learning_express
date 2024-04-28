4dbe24d28e114895e96fb22001fc2bd9a4e9a936
Rey Halsall <reduhq25@gmail.com>
2024-04-28 01:19:34 -0600

    Creating the endpoint to read all the Authors

---
+++ src/author/author.router.ts
@@ -0,0 +1,17 @@
+import express from "express"
+import {Request, Response} from "express"
+// import {body, validationResult} from "express-validator"
+
+import * as AuthorService from "./author.service"
+
+export const authorRouter = express.Router()
+
+// GET: List of all Authors
+authorRouter.get("/", async(_request:Request, response:Response) =>{
+    try{
+        const authors = await AuthorService.listAuthors()
+        return response.status(200).json(authors)
+    }catch(error:any){
+        return response.status(500).json(error.message)
+    }
+})
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 
+import {authorRouter} from "./author/author.router"
+
 const app = express()
 
 app.use(express.json()) // middleware que transforma la req.body a un JSON
+app.use("/api/authors", authorRouter)
 
 const PORT = 3000
 
